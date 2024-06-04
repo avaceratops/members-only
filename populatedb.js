46768f01743a7129cbd18af27a691f759e511d88
ava <148492613+avaceratops@users.noreply.github.com>
2024-06-04 12:13:38 +0100

    Add populatedb.js
    Adds sample messages to the database.

---
+++ populatedb.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+console.log('Populates the database with some default data');
+
+const Message = require('./models/message');
+const User = require('./models/user');
+
+const messages = [];
+
+// Set up mongoose connection
+mongoose.set('strictQuery', false);
+const mongoDB = process.env.MONGODB_URI;
+main().catch((err) => console.log(err));
+
+async function main() {
+  console.log('Debug: About to connect');
+  await mongoose.connect(mongoDB);
+  console.log('Debug: Should be connected?');
+  await createMessages();
+  console.log('Debug: Closing mongoose');
+  mongoose.connection.close();
+}
+
+async function messageCreate(index, title, text) {
+  const author = await User.findOne({ username: 'avaceratops' });
+  const message = new Message({ author, title, message: text });
+  await message.save();
+  messages[index] = message;
+  console.log(`Added message: ${title}`);
+}
+
+async function createMessages() {
+  console.log('Adding messages');
+  await Promise.all([messageCreate(0, 'Test #1', 'This is a message')]);
+  await Promise.all([messageCreate(1, 'Test #2', 'This is a message')]);
+  await Promise.all([messageCreate(2, 'Test #3', 'This is a message')]);
+}

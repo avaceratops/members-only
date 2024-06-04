adf4cd99d3a45648c9745a89b2a09147ecba09f4
ava <148492613+avaceratops@users.noreply.github.com>
2024-06-04 16:01:15 +0100

    Add functionality to add new messages

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,6 +1,8 @@
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 
 const Message = require('../models/message');
+const User = require('../models/user');
 
 // display list of all messages
 exports.message_list = asyncHandler(async (req, res) => {
@@ -10,3 +12,42 @@ exports.message_list = asyncHandler(async (req, res) => {
     .exec();
   res.render('index', { title: 'MessageBoard', messages });
 });
+
+exports.message_create_get = (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.redirect('/login');
+  }
+  return res.render('addMessage', { title: 'Add new message' });
+};
+
+exports.message_create_post = [
+  body('title').trim(),
+  body('msg')
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage('Message must not be empty'),
+
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    const { title, msg } = req.body;
+
+    // make sure the user is logged in
+    if (!req.isAuthenticated()) {
+      return res.redirect('/login');
+    }
+
+    const author = await User.findById(req.user._id);
+    const message = new Message({ author, title, msg });
+
+    if (!errors.isEmpty()) {
+      return res.render('addMessage', {
+        title: 'Add new message',
+        message,
+        errors: errors.mapped(),
+      });
+    }
+
+    await message.save();
+    return res.redirect('/');
+  }),
+];

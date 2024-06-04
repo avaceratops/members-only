16e5a294828c9cad5ee703e551d8543da6083215
ava <148492613+avaceratops@users.noreply.github.com>
2024-06-04 13:49:36 +0100

    Combine all existing routes into index route
    Current routes are relatively simple, and do not warrant the creation
    of individual routers.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ const express = require('express');
 const router = express.Router();
 
 const messageController = require('../controllers/messageController');
+const userController = require('../controllers/userController');
 
 router.get('/', messageController.message_list);
+
+router.get('/register', userController.user_create_get);
+router.post('/register', userController.user_create_post);
+
+router.get('/login', userController.user_login_get);
+router.post('/login', userController.user_login_post);
+
+router.get('/logout', userController.user_logout);
 
 module.exports = router;
